a4e4c8d922ca967142a6f1517a8ed871e3968b4f
Anastasiia Riabets <anastasiia.ria@gmail.com>
2022-02-21 14:32:03 -0800

    add showBoard function
    Co-authored-by: Marisa Edgar <marisaedgar1212@gmail.com>

--- queen-attack/Location/Queen.cs
+++ queen-attack/Location/Queen.cs
@@ -22,5 +22,37 @@ namespace Board.Location
       }
       return attack;
     }
+
+    public void ShowBoard(int anotherX, int anotherY)
+    {
+      for (int i = 0; i < 8; i++)
+      {
+        for (int j = 0; j < 8; j++)
+        {
+          string square = "_";
+          if (QueenX - 1 == i && QueenY - 1 == j)
+          {
+            square = "Q";
+          }
+          else if (anotherX - 1 == i && anotherY - 1 == j)
+          {
+            square = "A";
+          }
+
+          if (j == 0)
+          {
+            Console.Write("|" + square + "|");
+          }
+          else if (j == 7)
+          {
+            Console.WriteLine(square + "|");
+          }
+          else
+          {
+            Console.Write(square + "|");
+          }
+        }
+      }
+    }
   }
 }
--- queen-attack/Program.cs
+++ queen-attack/Program.cs
@@ -8,17 +8,22 @@ namespace Board
   {
     public static void Main()
     {
+      int anotherX = 0;
+      int anotherY = 0;
       Console.WriteLine("Enter queen X: ");
       int queenX = int.Parse(Console.ReadLine());
       Console.WriteLine("Enter queen Y: ");
       int queenY = int.Parse(Console.ReadLine());
 
-      Console.WriteLine("Enter another X: ");
-      int anotherX = int.Parse(Console.ReadLine());
-      Console.WriteLine("Enter another Y: ");
-      int anotherY = int.Parse(Console.ReadLine());
-
       Queen queen = new Queen(queenX, queenY);
+      queen.ShowBoard(anotherX, anotherY);
+
+      Console.WriteLine("Enter another X: ");
+      anotherX = int.Parse(Console.ReadLine());
+      Console.WriteLine("Enter another Y: ");
+      anotherY = int.Parse(Console.ReadLine());
+
+      queen.ShowBoard(anotherX, anotherY);
 
       bool attack = queen.QueenAttack(anotherX, anotherY);
       if (attack)
